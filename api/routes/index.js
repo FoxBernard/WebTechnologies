d38737ed4547158307390213bf3fd2a8d35d64b4
Bernard Fox <maniquest@hotmail.com>
2026-04-12 19:12:17 +0100

    Adding small texts for API routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'API working...',
+    endpoints: {
+      auth: "/auth",
+      comments: "/comments",
+      events: "/events",
+      invitations: "/invitations",
+      users: "/users"
+    }
+    });
 });
 
 module.exports = router;
